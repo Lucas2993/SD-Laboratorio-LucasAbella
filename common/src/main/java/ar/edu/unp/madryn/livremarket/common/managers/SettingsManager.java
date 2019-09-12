fb220f7676998645395980676a25ff9d474d6bd2
Lucas <lucas.gabriel2993@gmail.com>
2019-09-12 19:39:26 -0300

    Creacion del modulo comun y agregado de funcionalidad de configuracion
    - Se creo un modulo comun (common) que contiene todo el codigo comun a todos los servidores.
    
    - Se implemento la funcionalidad necesaria para poder utilizar archivos de tipo properties para configurar parametros de simulacion.
    
    - Se agrego apache-commons como dependencia.

---
+++ common/src/main/java/ar/edu/unp/madryn/livremarket/common/managers/SettingsManager.java
@@ -0,0 +1,67 @@
+package ar.edu.unp.madryn.livremarket.common.managers;
+
+import ar.edu.unp.madryn.livremarket.common.utils.FileGeneralUtils;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class SettingsManager {
+
+    private Properties settingsPropertiesContent;
+    private ClassLoader classLoader;
+
+    private static SettingsManager instance;
+
+    public static SettingsManager getInstance() {
+        if (instance == null) {
+            instance = new SettingsManager();
+        }
+        return instance;
+    }
+
+    private SettingsManager() {
+        this.classLoader = this.getClass().getClassLoader();
+    }
+
+    public boolean loadSettings(String fileName) {
+        File file = new File(fileName);
+        if (!FileGeneralUtils.fileExist(file)) {
+            InputStream resourceFileContent = this.classLoader.getResourceAsStream(fileName);
+            if (resourceFileContent == null) {
+                return false;
+            }
+
+            try {
+                FileUtils.copyInputStreamToFile(resourceFileContent, file);
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        this.settingsPropertiesContent = new Properties();
+
+        try {
+            InputStream input = new FileInputStream(file);
+
+            // load a properties file
+            this.settingsPropertiesContent.load(input);
+
+        } catch (IOException ex) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public String getSetting(String name) {
+        if (this.settingsPropertiesContent == null || this.settingsPropertiesContent.isEmpty()) {
+            return null;
+        }
+
+        return this.settingsPropertiesContent.getProperty(name);
+    }
+}
---
+++ common/src/main/java/ar/edu/unp/madryn/livremarket/common/utils/FileGeneralUtils.java
@@ -0,0 +1,15 @@
+package ar.edu.unp.madryn.livremarket.common.utils;
+
+import java.io.File;
+
+public class FileGeneralUtils {
+    public static boolean fileExist(String path) {
+        File file = new File(path);
+
+        return fileExist(file);
+    }
+
+    public static boolean fileExist(File file) {
+        return file.exists();
+    }
+}
