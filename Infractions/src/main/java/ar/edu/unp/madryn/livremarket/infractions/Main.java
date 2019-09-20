5a91d6bc22b8da92b93c2c641440a3958b5080eb
Lucas Abella <lucas.gabriel2993@gmail.com>
2019-09-19 22:15:12 -0300

    Procedimiento basico de recepcion de mensajes

--- Infractions/src/main/java/ar/edu/unp/madryn/livremarket/infractions/Main.java
+++ Infractions/src/main/java/ar/edu/unp/madryn/livremarket/infractions/Main.java
@@ -1,7 +1,39 @@
 package ar.edu.unp.madryn.livremarket.infractions;
 
+import ar.edu.unp.madryn.livremarket.common.configuration.ConfigurationManager;
+import ar.edu.unp.madryn.livremarket.common.configuration.ConfigurationSection;
+import ar.edu.unp.madryn.livremarket.common.messages.MessageDelivery;
+import ar.edu.unp.madryn.livremarket.common.messages.MessageServer;
+import ar.edu.unp.madryn.livremarket.common.messages.MessageServerFactory;
+
 public class Main {
     public static void main(String [] args){
+        // Configurar la conexion al servidor de mensajes.
+        ConfigurationManager configurationManager = ConfigurationManager.getInstance();
+        ConfigurationSection connectionConfiguration = configurationManager.loadConfiguration("connection", ConfigurationSection.CONFIGURATION_FOLDER);
 
+        MessageServerFactory messageServerFactory = MessageServerFactory.getInstance();
+        MessageServer messageServer = messageServerFactory.getNewMessageServer(connectionConfiguration);
+
+        // Establecer la conexion al servidor.
+        if(!messageServer.connect()){
+            System.err.println("No se pudo establecer conexion con el servidor AMQP!");
+            return;
+        }
+
+        // Escuchar todos los mensajes que vengan con el topico "*.infractions".
+        if(messageServer.registerProcessor("*.infractions", new MessageDelivery() {
+            @Override
+            public void processMessage(String consumerTag, String message) {
+                System.out.print("Mensaje recibido: ");
+                System.out.print("[" + consumerTag + "] ");
+                System.out.println(message);
+            }
+        })){
+            System.out.println("Escuchando mensajes (Ctrl + C para cerrar)...");
+        }
+        else{
+            System.err.println("No se pudo registrar el procesador de mensajes!");
+        }
     }
 }
