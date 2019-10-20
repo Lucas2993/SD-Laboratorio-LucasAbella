6c04a1919c0848b13b9cbf8efac5d21c2d9a3f2c
Lucas Abella <lucas.gabriel2993@gmail.com>
2019-10-20 20:50:07 -0300

    Cambios en la logica de la definicion de la maquina de estados
    - Se incorporo el uso de un Template para poder guardar el estado actual de una maquina y poder luego recuperarla sin necesidad de tener todo el objeto que contiene todas las definiciones y de mas.

--- common/src/main/java/ar/edu/unp/madryn/livremarket/common/sm/StateMachine.java
+++ common/src/main/java/ar/edu/unp/madryn/livremarket/common/sm/StateMachine.java
@@ -4,63 +4,28 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author iMinecrafting
  * Representa la organizacion y funcionamiento de una maquina de estados.
  */
 public class StateMachine {
-    private List<State> states;
-    private List<Transition> transitions;
+    private Template template;
     @Getter
     private State currentState;
     @Getter
     private List<State> history;
 
-    public StateMachine() {
-        this.states = new ArrayList<>();
-        this.transitions = new ArrayList<>();
+    public StateMachine(Template template) {
+        this.template = template;
         this.history = new ArrayList<>();
+        this.currentState = template.getInitialState();
     }
 
-    /**
-     * Agrega un estado a la maquina. Si la maquina aun no tiene estado actual, se setea.
-     * @param state Estado a agregar a la maquina.
-     * @return Verdadero si el estado pudo ser agregado correctamente.
-     */
-    public Boolean addState(State state) {
-        // Si el estado ya se encontraba registrado.
-        if (this.states.contains(state)) {
-            return false;
-        }
-
-        // Se agrega el estado.
-        this.states.add(state);
-        // Si no hay un estado actual aun.
-        if(this.currentState == null){
-            // Se setea el estado como el actual.
-            this.changeCurrentState(state);
-        }
-
-        return true;
-    }
-
-    /**
-     * Agrega una transicion a la maquina de estados.
-     * @param transition Transicion a agregar.
-     * @return Verdadero si la transicion pudo ser agregada correctamente.
-     */
-    public Boolean addTransition(Transition transition) {
-        // Si la maquina ya contenia la transicion.
-        if (this.transitions.contains(transition)) {
-            return false;
-        }
-
-        // Se agrega la transicion.
-        this.transitions.add(transition);
-
-        return true;
+    public StateMachine(Template template, State initialState) {
+        this.template = template;
+        this.history = new ArrayList<>();
+        this.changeCurrentState(initialState, false);
     }
 
     /**
@@ -74,7 +39,7 @@ public class StateMachine {
         }
 
         // Se evalua si existe una transicion posible partiendo del estado actual.
-        return this.hasTransition(this.currentState);
+        return this.template.hasTransition(this.currentState);
     }
 
     /**
@@ -88,7 +53,7 @@ public class StateMachine {
         }
 
         // Se busca una transicion que parta del estado actual y de la cual se cumpla la condicion.
-        Transition transition = this.searchTransition(this.currentState);
+        Transition transition = this.template.searchTransition(this.currentState);
         // Si se encontro una transicion.
         if (transition == null) {
             return false;
@@ -107,45 +72,34 @@ public class StateMachine {
      * @return Verdadero si fue posible cambiar de estado.
      */
     private Boolean changeCurrentState(State state) {
+        return this.changeCurrentState(state, true);
+    }
+
+    /**
+     * Realiza el cambio del estado actual hacia uno especifico.
+     * @param state Estado al cual se evoluciona.
+     * @param withinProcess Define si al cambiar de estado se realiza el proceso.
+     * @return Verdadero si fue posible cambiar de estado.
+     */
+    private Boolean changeCurrentState(State state, boolean withinProcess) {
         // Si no hay estado.
         if(state == null){
             return false;
         }
 
         // Si el estado no se encuentra registrado.
-        if (!this.states.contains(state)) {
-            // Se agrega el estado.
-            this.states.add(state);
+        if (!this.template.containsState(state)) {
+            return false;
         }
 
         // Se cambia el estado actual.
         this.currentState = state;
-        // Se ejecuta el proceso del nuevo estado.
-        this.currentState.process();
+        // Si no se desea realizar el proceso al realizar el cambio.
+        if(withinProcess) {
+            // Se ejecuta el proceso del nuevo estado.
+            this.currentState.process();
+        }
 
         return true;
-    }
-
-    /**
-     * Realiza la busqueda de una transicion mediante el estado de origen. En el proceso se evalua si la condicion se cumple.
-     * @param from Estado del cual se desea transicionar.
-     * @return La transicion encontrada. Si no se encontro una transicion valida, se devuelve null.
-     */
-    private Transition searchTransition(State from) {
-        Optional<Transition> result = this.transitions.stream()
-                .filter(transition -> transition.getFrom().equals(from) && transition.condition())
-                .findFirst();
-
-        return result.orElse(null);
-    }
-
-    /**
-     * Determina si hay una transicion que pueda realizarse desde el estado actual. En el proceso se evalua la condicion.
-     * @param from Estado del cual se desea transicionar.
-     * @return Verdadero si es posible transicionar.
-     */
-    private Boolean hasTransition(State from) {
-        return this.transitions.stream()
-                .anyMatch(transition -> transition.getFrom().equals(from) && transition.condition());
     }
 }
---
+++ common/src/main/java/ar/edu/unp/madryn/livremarket/common/sm/Template.java
@@ -0,0 +1,95 @@
+package ar.edu.unp.madryn.livremarket.common.sm;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author iMinecrafting
+ * Representa la organizacion (estados y transiciones) de una maquina de estados. Solo efectua la definicion de la misma.
+ */
+public class Template {
+    private List<State> states;
+    private List<Transition> transitions;
+    @Getter
+    private State initialState;
+
+    public Template() {
+        this.states = new ArrayList<>();
+        this.transitions = new ArrayList<>();
+    }
+
+    /**
+     * Agrega un estado a la maquina. Si la maquina aun no tiene estado actual, se setea.
+     * @param state Estado a agregar a la maquina.
+     * @return Verdadero si el estado pudo ser agregado correctamente.
+     */
+    public Boolean addState(State state) {
+        // Si el estado ya se encontraba registrado.
+        if (this.states.contains(state)) {
+            return false;
+        }
+
+        // Se agrega el estado.
+        this.states.add(state);
+        // Si no hay un estado inicial aun.
+        if(this.initialState == null){
+            // Se setea el estado como el inicial.
+            this.initialState = state;
+        }
+
+        return true;
+    }
+
+    /**
+     * Agrega una transicion a la maquina de estados.
+     * @param transition Transicion a agregar.
+     * @return Verdadero si la transicion pudo ser agregada correctamente.
+     */
+    public Boolean addTransition(Transition transition) {
+        // Si la maquina ya contenia la transicion.
+        if (this.transitions.contains(transition)) {
+            return false;
+        }
+
+        // Se agrega la transicion.
+        this.transitions.add(transition);
+
+        return true;
+    }
+
+    /**
+     * Realiza la busqueda de una transicion mediante el estado de origen. En el proceso se evalua si la condicion se cumple.
+     * @param from Estado del cual se desea transicionar.
+     * @return La transicion encontrada. Si no se encontro una transicion valida, se devuelve null.
+     */
+    Transition searchTransition(State from) {
+        Optional<Transition> result = this.transitions.stream()
+                .filter(transition -> transition.getFrom().equals(from) && transition.condition())
+                .findFirst();
+
+        return result.orElse(null);
+    }
+
+    /**
+     * Determina si hay una transicion que pueda realizarse desde el estado actual. En el proceso se evalua la condicion.
+     * @param from Estado del cual se desea transicionar.
+     * @return Verdadero si es posible transicionar.
+     */
+    Boolean hasTransition(State from) {
+        return this.transitions.stream()
+                .anyMatch(transition -> transition.getFrom().equals(from) && transition.condition());
+    }
+
+    /**
+     * Determina si la plantilla posee un estado dado dentro de su definicion.
+     * @param state Estado a buscar.
+     * @return Verdadero si contiene el estado dado. Falso en cualquier otro caso.
+     */
+    Boolean containsState(State state){
+        // Si el estado se encuentra registrado.
+        return this.states.contains(state);
+    }
+}
