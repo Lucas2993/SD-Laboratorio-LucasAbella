dcff3d0e7a837c06b26667f84f29d84821a66540
Lucas Abella <lucas.gabriel2993@gmail.com>
2019-11-05 14:43:45 -0300

    Purchases: Agregado de parametro de simulacion a la configuracion

--- Purchases/src/main/java/ar/edu/unp/madryn/livremarket/purchases/sm/SelectingDeliveryState.java
+++ Purchases/src/main/java/ar/edu/unp/madryn/livremarket/purchases/sm/SelectingDeliveryState.java
@@ -1,5 +1,6 @@
 package ar.edu.unp.madryn.livremarket.purchases.sm;
 
+import ar.edu.unp.madryn.livremarket.common.configuration.ConfigurationSection;
 import ar.edu.unp.madryn.livremarket.common.data.PurchaseManager;
 import ar.edu.unp.madryn.livremarket.common.messages.MessageCommonFields;
 import ar.edu.unp.madryn.livremarket.common.models.DeliveryDetail;
@@ -7,13 +8,17 @@ import ar.edu.unp.madryn.livremarket.common.models.DeliveryMethod;
 import ar.edu.unp.madryn.livremarket.common.models.Purchase;
 import ar.edu.unp.madryn.livremarket.common.sm.State;
 import lombok.Setter;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.Map;
 
 public class SelectingDeliveryState extends State {
+    private static final double DEFAULT_MAIL_PROBABILITY = 0.5;
 
     @Setter
     private PurchaseManager purchaseManager;
+    @Setter
+    private ConfigurationSection simulationConfiguration;
 
     public SelectingDeliveryState() {
         super("selecting_delivery");
@@ -41,7 +46,8 @@ public class SelectingDeliveryState extends State {
             return false;
         }
 
-        double deliveryProbability = 0.5;
+        String probabilityString = simulationConfiguration.getValue("mail_probability");
+        double deliveryProbability = NumberUtils.toDouble(probabilityString, DEFAULT_MAIL_PROBABILITY);
 
         DeliveryDetail deliveryDetail = new DeliveryDetail();
 
